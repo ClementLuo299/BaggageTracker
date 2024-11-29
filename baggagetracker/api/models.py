44df03f37ac204254f997e3441c1dcf22a75550f
Clement Luo <clementluo299@gmail.com>
2024-11-29 15:12:00 -0700

    created Usr model

--- baggagetracker/api/models.py
+++ baggagetracker/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+#Database relations
+
+#User, null=False is default
+class Usr(models.Model):
+    user_id = models.CharField(max_length=100, unique=True, primary_key=True)
+    fname = models.CharField(max_length=50)
+    mname = models.CharField(max_length=50,null=True)
+    lname = models.CharField(max_length=50)
+    street = models.CharField(max_length=100,null=True)
+    country = models.CharField(max_length=100,null=True)
+    postal_code = models.CharField(max_length=50,null=True)
+    email = models.CharField(max_length=50,null=True)
