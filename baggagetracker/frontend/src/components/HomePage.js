eb54fe912b8d9b1a03d6b1fa9a1848abc33f09e4
Clement Luo <clementluo299@gmail.com>
2024-11-29 21:07:55 -0700

    linked buttons

--- baggagetracker/frontend/src/components/HomePage.js
+++ baggagetracker/frontend/src/components/HomePage.js
@@ -13,32 +13,38 @@ export default class HomePage extends Component{
 
     render() {
         return (
-            <Grid container spacing={1}>
-                <Grid item xs={12} align="center">
-                    <Typography component ="h1" variant="h1">
-                        Baggage Tracker
-                    </Typography>
-                </Grid>
+            <Router>
+                <Grid container spacing={1} align="center">
+                    <Grid item xs={12}>
+                        <Typography component ="h1" variant="h1">
+                            Baggage Tracker
+                        </Typography>
+                    </Grid>
 
-                <Grid item xs={12} align="center">
-                    <Typography component ="h4" variant="h4">
-                        Select Login
-                    </Typography>
-                </Grid>
+                    <Grid item xs={12}>
+                        <Typography component ="h4" variant="h4">
+                            Select Login
+                        </Typography>
+                    </Grid>
 
-                <Grid item xs={12} align="center">
-                    <Button color="primary" variant="contained">
-                        Customer Login
-                    </Button>
-                </Grid>
+                    <Grid item xs={12}>
+                        <Button color="primary" variant="contained" href="/login">
+                            Customer Login
+                        </Button>
+                    </Grid>
 
-                
-                <Grid item xs={12} align="center">
-                    <Button color="secondary" variant="contained">
-                        Employee Login
-                    </Button>
+                    
+                    <Grid item xs={12}>
+                        <Button color="secondary" variant="contained" href ="/emplogin">
+                            Employee Login
+                        </Button>
+                    </Grid>
                 </Grid>
-            </Grid>
+                <Routes>
+                    <Route path="/login" element={<CustomerLogin/>}/>
+                    <Route path="/emplogin" element={<EmployeeLogin/>}/>
+                </Routes>
+            </Router>
         );
     }
 }
