f66c4e8632e684911fd7f225dbe842d26fc6048f
Clement Luo <clementluo299@gmail.com>
2024-12-01 23:05:45 -0700

    created unique dashboard for each user to display user id

--- baggagetracker/frontend/src/components/EmployeeDashboard.js
+++ baggagetracker/frontend/src/components/EmployeeDashboard.js
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
+import { useParams } from "react-router-dom";
 
-export default class EmployeeDashboard extends Component{
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <p>Dashboard for: User</p>
-        );
-    }
+const EmployeeDashboard = () => {
+    const {userID} = useParams();
+    return <p>Dashboard page for: {userID}</p>
 }
+
+export default EmployeeDashboard;
