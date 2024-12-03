82e739dcea56d9f02ac3566d725b20ebe8690f7b
Clement Luo <clementluo299@gmail.com>
2024-12-03 05:26:16 -0700

    created appbar for employee dashboard

--- baggagetracker/frontend/src/components/EmployeeDashboard.js
+++ baggagetracker/frontend/src/components/EmployeeDashboard.js
@@ -1,9 +1,60 @@
 import React, { Component } from "react";
 import { useParams } from "react-router-dom";
 
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import ToolbarGroup from "@material-ui/core/Toolbar";
+import{Button, Grid, Box, IconButton} from "@material-ui/core";
+import { AccountCircle } from "@mui/icons-material"
+
+const pages = ["Dashboard","Check In","Incidents"]
+
 const EmployeeDashboard = () => {
     const {userID} = useParams();
-    return <p>Dashboard page for: {userID}</p>
+
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+    const handleOpenNavMenu = (event) =>{
+        setAnchorElNav(event.currentTarget);
+    }
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    }
+
+    return (
+        <div>
+            <div>
+                <AppBar>
+                    <ToolBar>
+                        <ToolbarGroup firstChild={true} float = "left" display="flex" sx={{ml:"auto"}}>
+                            <IconButton>
+                                <AccountCircle style={{color:'white'}}/>
+                            </IconButton>
+
+                            <Button variant="text" color="inherit">
+                                Dashboard
+                            </Button>
+
+                            <Button variant="text" color="inherit" style={{whiteSpace:"nowrap"}}>
+                                Check In
+                            </Button>
+
+                            <Button variant="text" color="inherit">
+                                Incidents
+                            </Button>
+                        </ToolbarGroup>
+                    </ToolBar>
+                </AppBar>
+            </div>
+
+            <div>
+                <Button variant="contained" color="inherit">
+                    Content Here
+                </Button>
+            </div>
+        </div>
+    );
 }
 
 export default EmployeeDashboard;
